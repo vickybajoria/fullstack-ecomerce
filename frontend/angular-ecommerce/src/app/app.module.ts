5a37e88a1e962375ae0b02071c869d45b30a2a18
Vicky Bajoria <vickybajoria@gmail.com>
2020-04-06 09:21:52 +0530

    initial shoping cart feature The following changes are made in this commit: - cart-item class is added - cart-status component is added which subscribe total price and total   quantity of cart service - cart service is added

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -30,14 +30,14 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule,
-    FontAwesomeModule
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
